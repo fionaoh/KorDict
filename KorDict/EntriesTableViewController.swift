f95f07f513715ef7bd754a086ae7ffd5a10ea3ba
Fiona Oh <fionaoh2002@gmail.com>
2021-07-07 17:15:32 -0700

    set up tableVC with two hard-coded entries that are now visible (after previously NOT working for some unknown reason..........)

---
+++ KorDict/EntriesTableViewController.swift
@@ -0,0 +1,66 @@
+//
+//  EntriesTableViewController.swift
+//  KorDict
+//
+//  Created by Fiona Oh on 7/7/21.
+//
+
+import UIKit
+
+class EntriesTableViewController: UITableViewController {
+    
+    var entries : [Entry] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        entries = createEntries()
+    }
+    
+    func createEntries() -> [Entry] {
+        let entry1 = Entry()
+        entry1.term = "불행"
+        entry1.translation = "unhappiness"
+        entry1.isStarred = true
+        
+        let entry2 = Entry()
+        entry2.term = "뱃사람"
+        entry2.translation = "sailor"
+        
+        return [entry1, entry2]
+    }
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return entries.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        let entry = entries[indexPath.row]
+        print(entry.term)
+        
+        if entry.isStarred {
+            cell.textLabel?.text = "⭐️ " + entry.term
+        } else {
+            cell.textLabel?.text = entry.term
+        }
+
+        return cell
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ KorDict/Entry.swift
@@ -0,0 +1,22 @@
+//
+//  Entry.swift
+//  KorDict
+//
+//  Created by Fiona Oh on 7/7/21.
+//
+
+import UIKit
+
+class Entry {
+    var term = ""
+    var translation = ""
+    var notes = ""
+    var isStarred = false
+   
+    /*
+    init(termString : String, translationString : String, notesString : String) {
+        term = termString
+        translation = translationString
+        notes = notesString
+    }*/
+}
